22b82a821917f58a89d8e1d0bc708009d2fa29da
rdenysov <romandenysov9@gmail.com>
2023-01-03 10:50:15 +0200

    MONOPOLY: add configuration for properties

--- src/main/java/com/sokima/monopoly/MonopolyApplication.java
+++ src/main/java/com/sokima/monopoly/MonopolyApplication.java
@@ -2,8 +2,12 @@ package com.sokima.monopoly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
 public class MonopolyApplication {
 
     public static void main(String[] args) {
