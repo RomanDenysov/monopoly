54a54a16e58c006246595ec8af4f0c7273b56d94
rdenysov <romandenysov9@gmail.com>
2023-01-03 07:52:14 +0200

    MONOPOLY: add dice

---
+++ src/main/java/com/sokima/monopoly/model/dice/Dice.java
@@ -0,0 +1,19 @@
+package com.sokima.monopoly.model.dice;
+
+import lombok.Getter;
+
+import java.util.Random;
+
+@Getter
+public class Dice {
+
+    /**
+     * dice value between 1 and 6
+     */
+    private final Integer diceValue;
+
+    public Dice() {
+        // generate random value after create obj
+        this.diceValue = new Random().nextInt(6) + 1;
+    }
+}
