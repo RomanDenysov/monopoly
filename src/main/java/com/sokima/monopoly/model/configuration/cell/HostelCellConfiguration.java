c4d3779e96664de758e4372de3e26779b8b1c726
rdenysov <romandenysov9@gmail.com>
2023-01-03 12:16:38 +0200

    MONOPOLY: add conf classes for cell

---
+++ src/main/java/com/sokima/monopoly/model/configuration/cell/HostelCellConfiguration.java
@@ -0,0 +1,47 @@
+package com.sokima.monopoly.model.configuration.cell;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "monopoly.cell.hostel")
+public class HostelCellConfiguration {
+
+    private FirstCompany firstCompany = new FirstCompany();
+
+    private SecondCompany secondCompany = new SecondCompany();
+
+    private ThirdCompany thirdCompany = new ThirdCompany();
+
+    @Getter
+    @Setter
+    public static class FirstCompany {
+        private Long price = 1000L;
+
+        private Long rent = 100L;
+
+        private String businessName;
+    }
+
+    @Getter
+    @Setter
+    public static class SecondCompany {
+        private Long price = 1100L;
+
+        private Long rent = 110L;
+
+        private String businessName;
+    }
+
+    @Getter
+    @Setter
+    public static class ThirdCompany {
+        private Long price = 1200L;
+
+        private Long rent = 120L;
+
+        private String businessName;
+    }
+}
