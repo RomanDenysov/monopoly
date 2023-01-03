67d8e3166ff43b1cff3df07f43f55a8de604a121
rdenysov <romandenysov9@gmail.com>
2023-01-03 07:52:49 +0200

    MONOPOLY: add utility classes for manipulating dice

---
+++ src/main/java/com/sokima/monopoly/util/PositionManipulator.java
@@ -0,0 +1,34 @@
+package com.sokima.monopoly.util;
+
+import com.sokima.monopoly.model.dice.Dice;
+
+import java.util.List;
+
+/**
+ * Util class for calculating next position on the desk
+ */
+public final class PositionManipulator {
+
+    private PositionManipulator() {
+
+    }
+
+    /**
+     * calculate next position number using {@link List<Dice>} values
+     *
+     * @param currentPosition current position on desk
+     * @param dices the generated dices
+     * @param numberOfCells cells count
+     * @return next position number
+     */
+    public static Integer nextPosition(Integer currentPosition, List<Dice> dices, Integer numberOfCells) {
+        Integer position = currentPosition;
+
+        for (Dice dice : dices) {
+            position += dice.getDiceValue();
+        }
+
+        position = position % numberOfCells;
+        return position;
+    }
+}
