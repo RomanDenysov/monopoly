841938ab78b2b5f1d98e47216962fc0df15d436d
Sokima <romandenysov9@gmail.com>
2023-01-08 15:41:37 +0200

    MONOPOLY: add util classes for test

---
+++ src/test/java/com/sokima/monopoly/util/CellGeneratorUtil.java
@@ -0,0 +1,34 @@
+package com.sokima.monopoly.util;
+
+import com.sokima.monopoly.model.cell.Cell;
+import com.sokima.monopoly.model.cell.business.AirportBusiness;
+import com.sokima.monopoly.model.cell.business.BusinessCell;
+
+public final class CellGeneratorUtil {
+
+    private CellGeneratorUtil() {
+    }
+
+    public static Cell generateEmptyCell() {
+        return new Cell() {
+        };
+    }
+
+    public static BusinessCell generateBusinessCellBank() {
+        BusinessCell businessCell = new AirportBusiness();
+        businessCell.setOwner(PlayerGeneratorUtil.generateEmptyBank());
+        return businessCell;
+    }
+
+    public static BusinessCell generateBusinessCellPlayer() {
+        BusinessCell businessCell = new AirportBusiness();
+        businessCell.setOwner(PlayerGeneratorUtil.generateBusinessmanOwner());
+        return businessCell;
+    }
+
+    public static BusinessCell generateBusinessCellPlayer(Long rent) {
+        BusinessCell businessCell = generateBusinessCellPlayer();
+        businessCell.setRent(rent);
+        return businessCell;
+    }
+}
---
+++ src/test/java/com/sokima/monopoly/util/PlayerGeneratorUtil.java
@@ -0,0 +1,44 @@
+package com.sokima.monopoly.util;
+
+import com.sokima.monopoly.model.player.Bank;
+import com.sokima.monopoly.model.player.Businessman;
+
+public final class PlayerGeneratorUtil {
+
+    private PlayerGeneratorUtil() {
+    }
+
+    public static Businessman generateEmptyBusinessman() {
+        return new Businessman();
+    }
+
+    public static Bank generateEmptyBank() {
+        return new Bank();
+    }
+
+    public static Businessman generateBusinessmanOwner() {
+        Businessman businessman = new Businessman();
+        businessman.setName(KindOfPlayer.BUSINESSMAN_OWNER.name());
+        return businessman;
+    }
+
+    public static Businessman generateBusinessman() {
+        Businessman businessman = new Businessman();
+        businessman.setName(KindOfPlayer.BUSINESSMAN.name());
+        return businessman;
+    }
+
+    public static Businessman generateBusinessman(Long balance) {
+        Businessman businessman = generateBusinessman();
+        businessman.setBalance(balance);
+        return businessman;
+    }
+
+    private enum KindOfPlayer {
+        BUSINESSMAN_OWNER,
+        BUSINESSMAN,
+
+        BANK_OWNER,
+        BANK
+    }
+}
